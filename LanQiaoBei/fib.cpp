1bef1e7ab52da142a039ca2135a31f0006c0a152
ZOHFMKII <1076309717@qq.com>
2023-03-07 06:12:03 -0800

    lanqiaobei--ling lei de fib wen ti

---
+++ LanQiaoBei/fib.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int Fib(int n){
+	if(n <= 0){
+		return 0;
+	}
+	if(n ==1 || n == 2){
+		return 1;
+	}
+	int a, b;
+	a = 1, b = 1;
+	for(int i = 3; i < n; i++){
+		int c = (a + b) % 10007;
+		a = b;
+		b = c;
+	}
+	return b;
+}
+
+int main(){
+	int a =0;
+	cin >> a;
+	cout << Fib(a) << " ";
+	return 0;
+}
